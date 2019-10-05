e8e628bd85a21e27ee2990008ff23586c60e77ab
wholenote <brianwang2018@gmail.com>
2019-10-05 18:32:32 -0500

    Added player 1 energy and feathers display

---
+++ PillowFight/Assets/UI/energydisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class energydisplay : MonoBehaviour
+{
+    private int ener = 25;
+    public Text energyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energyText.text = "ENERGY: " + ener;
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            ener--;
+        }
+    }
+}
---
+++ PillowFight/Assets/UI/feathers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class feathers : MonoBehaviour
+{
+
+
+    private int feath = 25;
+    public Text featherText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        featherText.text = "FEATHERS: " + feath;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            feath--;
+        }
+    }
+}
